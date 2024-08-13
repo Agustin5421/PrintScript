5374d28ab2d956d9e86d62f1a70c78170619e971
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-08-13 11:52:13 -0300

    Progress on the interpreter to recognize the println method. Added methods to VariablesRepository for this intend.

--- interpreter/src/main/java/interpreter/Interpreter.java
+++ interpreter/src/main/java/interpreter/Interpreter.java
@@ -1,8 +1,11 @@
 package interpreter;
 
-import ast.Literal;
-import ast.Program;
-import ast.ASTNode;
+import ast.*;
+import ast.expressions.CallExpression;
+import ast.expressions.ExpressionStatement;
+import ast.expressions.ExpressionType;
+
+import java.util.List;
 
 public class Interpreter {
     public VariablesRepository executeProgram(Program program) {
@@ -24,8 +27,35 @@ public class Interpreter {
             Object value = literal.getValue();
 
             return variablesRepository.addVariable(name, value);
-        } else {
-            return variablesRepository;
+        } else if (statement instanceof ExpressionStatement) {
+            ExpressionStatement expressionStatement = (ExpressionStatement) statement;
+            ExpressionType expressionType = expressionStatement.getExpressionType();
+
+            if (expressionType instanceof CallExpression) {
+                CallExpression callExpression = (CallExpression) expressionType;
+                Identifier identifier = callExpression.getIdentifier();
+                List<Expression> arguments = callExpression.getArguments();
+                boolean optionalParameters = callExpression.hasOptionalParameters();
+
+                if (identifier.getName().equals("println")) {
+                    for (Expression argument : arguments) {
+                        if (argument instanceof StringLiteral) {
+                            StringLiteral stringLiteral = (StringLiteral) argument;
+                            System.out.print(stringLiteral.getValue());
+                        } else if (argument instanceof NumberLiteral) {
+                            NumberLiteral numberLiteral = (NumberLiteral) argument;
+                            System.out.print(numberLiteral.getValue());
+                        } else if (argument instanceof Identifier) {
+                            Identifier identifierArgument = (Identifier) argument;
+                            System.out.print(variablesRepository.getVariable(identifierArgument.getName()));
+                        }
+                    }
+                    System.out.println();
+                }
+
+                return variablesRepository;
+            }
         }
+        return variablesRepository;
     }
 }
