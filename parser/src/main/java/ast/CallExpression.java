5d26ee2b1afa790962114d0ccb5e83d827f3c537
JuliIglesias <jiglesias@mail.austral.edu.ar>
2024-08-13 13:11:08 -0300

    records implementations

--- parser/src/main/java/ast/CallExpression.java
+++ parser/src/main/java/ast/CallExpression.java
@@ -4,33 +4,5 @@ import token.Position;
 
 import java.util.List;
 
-public class CallExpression implements Expression{
-    private final Identifier callee;
-    private final List<Literal> arguments;
-    private final Position start;
-    private final Position end;
-
-    public CallExpression(Identifier callee, List<Literal> arguments, Position start, Position end) {
-        this.callee = callee;
-        this.arguments = arguments;
-        this.start = start;
-        this.end = end;
-    }
-
-    public Identifier getCallee() {
-        return callee;
-    }
-
-    public List<Literal> getArguments() {
-        return arguments;
-    }
-
-    public Position getStart() {
-        return start;
-    }
-
-    public Position getEnd() {
-        return end;
-    }
-
+public record CallExpression(Identifier callee, List<Literal> arguments, Position start, Position end) implements Expression {
 }
