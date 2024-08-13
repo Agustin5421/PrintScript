2603f672c47b47cb81e8e3c4bc945a276803d6d4
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-08-13 00:12:37 -0300

    Interpreter progress: Only saving variables

---
+++ interpreter/src/main/java/interpreter/Interpreter.java
@@ -0,0 +1,29 @@
+package interpreter;
+
+import ast.Literal;
+import ast.Program;
+import ast.ASTNode;
+
+public class Interpreter {
+    public void executeProgram(Program program) {
+        VariablesRepository variablesRepository = new VariablesRepository();
+
+        for (ASTNode statement : program.getStatements()) {
+            variablesRepository = evaluateStatement(statement, variablesRepository);
+        }
+    }
+
+    private VariablesRepository evaluateStatement(ASTNode statement, VariablesRepository variablesRepository) {
+        if (statement instanceof ast.VariableDeclaration) {
+            ast.VariableDeclaration variableDeclaration = (ast.VariableDeclaration) statement;
+
+            String name = variableDeclaration.getIdentifier().getName();
+            Literal literal = variableDeclaration.getLiteral();
+            Object value = literal.getValue();
+
+            return variablesRepository.addVariable(name, value);
+        } else {
+            return variablesRepository;
+        }
+    }
+}
