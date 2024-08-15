796224bc77e686effb1fcfc8287d4039d13360a5
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-08-14 22:53:51 -0300

    First try (doesn't work)

--- interpreter/build.gradle.kts
+++ interpreter/build.gradle.kts
@@ -12,8 +12,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation(project(":lexer"))
     implementation(project(":parser"))
-    implementation(project(":lexer")) //wtf la transitividad no funca
 }
 
 tasks.test {
