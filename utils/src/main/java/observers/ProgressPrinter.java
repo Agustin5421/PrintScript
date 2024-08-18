a686786fad23727bac7e6ce30e887ce1e317ad6d
Agustin <almartinez1@mail.austral.edu.ar>
2024-08-18 11:27:21 -0300

    implemented loading bar

---
+++ utils/src/main/java/observers/ProgressPrinter.java
@@ -0,0 +1,25 @@
+package observers;
+
+import printer.ProgressType;
+
+public class ProgressPrinter {
+    private static final int BAR_WIDTH = 50;
+
+    public void printProgress(int progress, ProgressType type) {
+        int completedWidth = (progress * BAR_WIDTH) / 100;
+        StringBuilder sb = new StringBuilder();
+        String color = type.getColorCode();
+
+        sb.append(color);
+        sb.append("[");
+        for (int i = 0; i < BAR_WIDTH; i++) {
+            if (i < completedWidth) {
+                sb.append("=");
+            } else {
+                sb.append(" ");
+            }
+        }
+        sb.append("] ").append(progress).append("%\033[0m\r");
+        System.out.print(sb);
+    }
+}
---
+++ utils/src/main/java/printer/ProgressType.java
@@ -0,0 +1,17 @@
+package printer;
+
+public enum ProgressType {
+    IN_PROGRESS("\033[34m"),  // Blue
+    COMPLETED("\033[32m"),    // Green
+    ERROR("\033[31m");        // RED
+
+    private final String colorCode;
+
+    ProgressType(String colorCode) {
+        this.colorCode = colorCode;
+    }
+
+    public String getColorCode() {
+        return colorCode;
+    }
+}
