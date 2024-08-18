97103f8ffd51a020e70f6ff6ff0e3b8b531db8eb
Agustin <almartinez1@mail.austral.edu.ar>
2024-08-18 11:27:10 -0300

    implemented observers

---
+++ utils/src/main/java/observers/ProgressObserver.java
@@ -0,0 +1,42 @@
+package observers;
+
+import printer.ProgressType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProgressObserver  {
+    private final ProgressPrinter progressPrinter;
+    private final Map<String, Integer> moduleProgress;
+    private final Map<String, Integer> modulePercentages;
+
+    public ProgressObserver(ProgressPrinter progressPrinter) {
+        this.progressPrinter = progressPrinter;
+        this.moduleProgress = new HashMap<>();
+        this.modulePercentages = new HashMap<>();
+    }
+
+    public void registerModule(String moduleName, int percentage) {
+        modulePercentages.put(moduleName, percentage);
+        moduleProgress.put(moduleName, 0);
+    }
+
+    public void update(String moduleName, int progress) {
+        int percentage = modulePercentages.getOrDefault(moduleName, 0);
+        int progressValue = (progress * percentage) / 100;
+        moduleProgress.put(moduleName, progressValue);
+
+        int totalProgress = moduleProgress.values().stream().mapToInt(Integer::intValue).sum();
+        int totalPercentage = Math.min(totalProgress, 100); // Ensure total percentage does not exceed 100%
+
+        progressPrinter.printProgress(totalPercentage, ProgressType.IN_PROGRESS);
+    }
+
+    public void finish() {
+        progressPrinter.printProgress(100, ProgressType.COMPLETED);
+    }
+
+    public void error(int progress) {
+        progressPrinter.printProgress(progress, ProgressType.ERROR);
+    }
+}
