a5c87318214198da070a7230652dba1560a6e65d
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-08-27 11:26:15 -0300

    Refactored Report attributes.

--- linter/src/main/java/linter/rework/report/Report.java
+++ linter/src/main/java/linter/rework/report/Report.java
@@ -2,9 +2,9 @@ package linter.rework.report;
 
 import token.Position;
 
-public record Report(Position initialPosition, Position finalPosition, String message) {
+public record Report(Position start, Position end, String message) {
   @Override
   public String toString() {
-    return "Warning from " + initialPosition + " to " + finalPosition + ": " + message;
+    return "Warning from " + start + " to " + end + ": " + message;
   }
 }
