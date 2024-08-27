9886fa78c9e21cd9b3b8a8ef23d7ee2380d6ec28
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-08-27 11:26:51 -0300

    Tested main IdentifierLintingStrategy.

--- linter/src/test/java/linter/strategy/identifier/CamelCaseTest.java
+++ linter/src/test/java/linter/strategy/identifier/CamelCaseTest.java
@@ -1,22 +1,23 @@
-package linter.strategy;
+package linter.strategy.identifier;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import ast.identifier.Identifier;
 import linter.rework.report.FullReport;
+import linter.rework.report.Report;
 import linter.rework.visitor.strategy.LintingStrategy;
-import linter.rework.visitor.strategy.identifier.SnakeCaseIdentifier;
+import linter.rework.visitor.strategy.identifier.CamelCaseIdentifier;
 import org.junit.jupiter.api.Test;
 import token.Position;
 
-public class SnakeCaseTest {
+public class CamelCaseTest {
   @Test
-  public void identifierIsInSnakeCase() {
+  public void identifierIsInCamelCase() {
     Position start = new Position(0, 0);
     Position end = new Position(0, 8);
-    Identifier identifier = new Identifier("test_name", start, end);
+    Identifier identifier = new Identifier("testName", start, end);
 
-    LintingStrategy<Identifier> camelCaseIdentifier = new SnakeCaseIdentifier();
+    LintingStrategy<Identifier> camelCaseIdentifier = new CamelCaseIdentifier();
     FullReport fullReport = new FullReport();
     fullReport = camelCaseIdentifier.apply(identifier, fullReport);
 
@@ -24,15 +25,19 @@ public class SnakeCaseTest {
   }
 
   @Test
-  public void identifierIsNotInSnakeCase() {
+  public void identifierIsNotInCamelCase() {
     Position start = new Position(0, 0);
     Position end = new Position(0, 8);
-    Identifier identifier = new Identifier("testName", start, end);
+    Identifier identifier = new Identifier("test_name", start, end);
 
-    LintingStrategy<Identifier> camelCaseIdentifier = new SnakeCaseIdentifier();
+    LintingStrategy<Identifier> camelCaseIdentifier = new CamelCaseIdentifier();
     FullReport fullReport = new FullReport();
     fullReport = camelCaseIdentifier.apply(identifier, fullReport);
 
     assertEquals(1, fullReport.getReports().size());
+
+    Report report = fullReport.getReports().get(0);
+    assertEquals(start, report.start());
+    assertEquals(end, report.end());
   }
 }
---
+++ linter/src/test/java/linter/strategy/identifier/IdentifierLintingTest.java
@@ -0,0 +1,78 @@
+package linter.strategy.identifier;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import ast.identifier.Identifier;
+import java.util.List;
+import linter.rework.report.FullReport;
+import linter.rework.visitor.strategy.LintingStrategy;
+import linter.rework.visitor.strategy.identifier.CamelCaseIdentifier;
+import linter.rework.visitor.strategy.identifier.IdentifierLintingStrategy;
+import linter.rework.visitor.strategy.identifier.SnakeCaseIdentifier;
+import org.junit.jupiter.api.Test;
+import token.Position;
+
+public class IdentifierLintingTest {
+  private List<Identifier> getIdentifiers() {
+    Position camelStart = new Position(0, 0);
+    Position camelEnd = new Position(0, 8);
+    Identifier camelIdentifier = new Identifier("testName", camelStart, camelEnd);
+
+    Position snakeStart = new Position(1, 0);
+    Position snakeEnd = new Position(1, 8);
+    Identifier snakeIdentifier = new Identifier("test_name", snakeStart, snakeEnd);
+
+    return List.of(camelIdentifier, snakeIdentifier);
+  }
+
+  @Test
+  public void severalIdentifiersCamelCaseTest() {
+    List<Identifier> identifiers = getIdentifiers();
+
+    LintingStrategy<Identifier> camelCaseIdentifier = new CamelCaseIdentifier();
+    //    LintingStrategy<Identifier> snakeCaseIdentifier = new SnakeCaseIdentifier();
+    LintingStrategy<Identifier> mainIdLinting =
+        new IdentifierLintingStrategy(List.of(camelCaseIdentifier));
+
+    FullReport fullReport = new FullReport();
+    for (Identifier identifier : identifiers) {
+      fullReport = mainIdLinting.apply(identifier, fullReport);
+    }
+
+    assertEquals(1, fullReport.getReports().size());
+  }
+
+  @Test
+  public void severalIdentifiersSnakeCaseTest() {
+    List<Identifier> identifiers = getIdentifiers();
+
+    //    LintingStrategy<Identifier> camelCaseIdentifier = new CamelCaseIdentifier();
+    LintingStrategy<Identifier> snakeCaseIdentifier = new SnakeCaseIdentifier();
+    LintingStrategy<Identifier> mainIdLinting =
+        new IdentifierLintingStrategy(List.of(snakeCaseIdentifier));
+
+    FullReport fullReport = new FullReport();
+    for (Identifier identifier : identifiers) {
+      fullReport = mainIdLinting.apply(identifier, fullReport);
+    }
+
+    assertEquals(1, fullReport.getReports().size());
+  }
+
+  @Test
+  public void severalIdentifiersBothCaseTest() {
+    List<Identifier> identifiers = getIdentifiers();
+
+    LintingStrategy<Identifier> camelCaseIdentifier = new CamelCaseIdentifier();
+    LintingStrategy<Identifier> snakeCaseIdentifier = new SnakeCaseIdentifier();
+    LintingStrategy<Identifier> mainIdLinting =
+        new IdentifierLintingStrategy(List.of(camelCaseIdentifier, snakeCaseIdentifier));
+
+    FullReport fullReport = new FullReport();
+    for (Identifier identifier : identifiers) {
+      fullReport = mainIdLinting.apply(identifier, fullReport);
+    }
+
+    assertEquals(2, fullReport.getReports().size());
+  }
+}
