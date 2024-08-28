23ce4aadfcb9a6bbcb8f0c2635f40636982e844a
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-08-28 13:21:28 -0300

    Implemented LinterV2.

--- linter/src/main/java/linter/rework/LinterV2.java
+++ linter/src/main/java/linter/rework/LinterV2.java
@@ -1,10 +1,24 @@
 package linter.rework;
 
+import ast.root.AstNode;
 import ast.root.Program;
+import ast.visitor.NodeVisitor;
+import java.util.List;
 import linter.rework.report.FullReport;
+import linter.rework.visitor.LinterVisitorFactory;
+import linter.rework.visitor.LinterVisitorV2;
 
 public class LinterV2 {
-  public FullReport lint(Program program) {
-    return null;
+  private final LinterVisitorFactory linterVisitorFactory = new LinterVisitorFactory();
+
+  public FullReport lint(Program program, String rules) {
+    NodeVisitor linterVisitor = linterVisitorFactory.createLinterVisitor(rules);
+
+    List<AstNode> statements = program.statements();
+    for (AstNode statement : statements) {
+      linterVisitor = statement.accept(linterVisitor);
+    }
+
+    return ((LinterVisitorV2) linterVisitor).getFullReport();
   }
 }
