70cb1cf0117eb885c8a6c1b1769ba15872f9f1da
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-08-28 13:21:54 -0300

    Tested module.

---
+++ linter/src/test/java/linter/visitor/ComplexLinterVisitorTest.java
@@ -0,0 +1,57 @@
+package linter.visitor;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import ast.identifier.Identifier;
+import ast.literal.StringLiteral;
+import ast.root.Program;
+import ast.statements.CallExpression;
+import ast.statements.VariableDeclaration;
+import java.util.List;
+import linter.TestUtils;
+import linter.rework.LinterV2;
+import linter.rework.report.FullReport;
+import org.junit.jupiter.api.Test;
+import token.Position;
+
+public class ComplexLinterVisitorTest {
+  private Program getProgram() {
+    Position position = new Position(0, 0);
+    Identifier identifier = new Identifier("testName", position, position);
+    StringLiteral stringLiteral = new StringLiteral("test", position, position);
+    VariableDeclaration variableDeclaration =
+        new VariableDeclaration(identifier, stringLiteral, position, position);
+
+    CallExpression callExpression =
+        new CallExpression(identifier, List.of(identifier, stringLiteral), true);
+
+    return new Program(List.of(variableDeclaration, callExpression));
+  }
+
+  @Test
+  public void noViolationsTest() {
+    Program program = getProgram();
+
+    String rules = TestUtils.readResourceFile("linterRulesExample.json");
+    assertNotNull(rules);
+
+    LinterV2 linter = new LinterV2();
+    FullReport report = linter.lint(program, rules);
+
+    assertEquals(0, report.getReports().size());
+  }
+
+  @Test
+  public void violationsTest() {
+    Program program = getProgram();
+
+    String rules = TestUtils.readResourceFile("anotherLinterRulesExample.json");
+    assertNotNull(rules);
+
+    LinterV2 linter = new LinterV2();
+    FullReport report = linter.lint(program, rules);
+
+    assertEquals(4, report.getReports().size());
+  }
+}
