17a0d09ecd54c1d3a8029bdbda403433ccd62df3
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-08-28 13:37:38 -0300

    Added original linter methods/functionality to V2.

--- linter/src/main/java/linter/rework/LinterV2.java
+++ linter/src/main/java/linter/rework/LinterV2.java
@@ -7,18 +7,55 @@ import java.util.List;
 import linter.rework.report.FullReport;
 import linter.rework.visitor.LinterVisitorFactory;
 import linter.rework.visitor.LinterVisitorV2;
+import observers.Observer;
+import observers.Progressable;
 
-public class LinterV2 {
+public class LinterV2 implements Progressable {
   private final LinterVisitorFactory linterVisitorFactory = new LinterVisitorFactory();
+  private final List<Observer> observers;
+  private int totalSteps;
+
+  public LinterV2() {
+    this.observers = List.of();
+  }
+
+  public LinterV2(List<Observer> observer) {
+    this.observers = observer;
+  }
 
   public FullReport lint(Program program, String rules) {
     NodeVisitor linterVisitor = linterVisitorFactory.createLinterVisitor(rules);
 
     List<AstNode> statements = program.statements();
+    totalSteps = statements.size();
+
     for (AstNode statement : statements) {
       linterVisitor = statement.accept(linterVisitor);
+      notifyObservers();
     }
 
     return ((LinterVisitorV2) linterVisitor).getFullReport();
+  }
+
+  @Override
+  public float getProgress() {
+    return (float) 1 / totalSteps * 100;
+  }
+
+  @Override
+  public void addObserver(Observer observer) {
+    observers.add(observer);
+  }
+
+  @Override
+  public void removeObserver(Observer observer) {
+    observers.remove(observer);
+  }
+
+  @Override
+  public void notifyObservers() {
+    for (Observer observer : observers) {
+      observer.update(this);
+    }
   }
 }
