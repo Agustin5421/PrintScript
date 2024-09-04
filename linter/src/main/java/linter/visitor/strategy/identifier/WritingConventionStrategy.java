5e3d39cfc3721fbf4fc451e3ae10a8b5e165cffd
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-09-04 14:37:03 -0300

    Created a default WritingConventionStrategy class that abstracts the writing convention to apply. Tested it.

---
+++ linter/src/main/java/linter/visitor/strategy/identifier/WritingConventionStrategy.java
@@ -0,0 +1,42 @@
+package linter.visitor.strategy.identifier;
+
+import ast.identifier.Identifier;
+import ast.root.AstNode;
+import ast.root.AstNodeType;
+import linter.report.FullReport;
+import linter.report.Report;
+import linter.visitor.strategy.LintingStrategy;
+
+public class WritingConventionStrategy implements LintingStrategy {
+  private final String writingConventionName;
+  private final String writingConventionPattern;
+
+  public WritingConventionStrategy(String writingConventionName, String writingConventionPattern) {
+    this.writingConventionName = writingConventionName;
+    this.writingConventionPattern = writingConventionPattern;
+  }
+
+  @Override
+  public FullReport apply(AstNode node, FullReport fullReport) {
+    if (!shouldApply(node)) {
+      return fullReport;
+    }
+
+    Identifier identifier = (Identifier) node;
+
+    if (!identifier.name().matches(writingConventionPattern)) {
+      fullReport =
+          fullReport.addReport(
+              new Report(
+                  identifier.start(),
+                  identifier.end(),
+                  "Identifier " + identifier.name() + " is not in " + writingConventionName));
+    }
+
+    return fullReport;
+  }
+
+  private boolean shouldApply(AstNode node) {
+    return node.getType().equals(AstNodeType.IDENTIFIER);
+  }
+}
