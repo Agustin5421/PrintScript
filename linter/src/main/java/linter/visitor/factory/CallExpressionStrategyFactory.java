e2235168d1f808664f534a9f074e2c5f78464edf
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-09-05 17:34:45 -0300

    Created a factory for the CallExpression

---
+++ linter/src/main/java/linter/visitor/factory/CallExpressionStrategyFactory.java
@@ -0,0 +1,52 @@
+package linter.visitor.factory;
+
+import ast.root.AstNodeType;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.util.ArrayList;
+import java.util.List;
+import linter.visitor.strategy.LintingStrategy;
+import linter.visitor.strategy.StrategiesContainer;
+import linter.visitor.strategy.callexpression.ArgumentsStrategy;
+
+public class CallExpressionStrategyFactory implements StrategyFactory {
+  @Override
+  public LintingStrategy createStrategies(String rules) {
+    JsonObject jsonObject =
+        JsonParser.parseString(rules).getAsJsonObject().getAsJsonObject("callExpression");
+
+    LintingStrategy validArguments = getValidArguments(jsonObject);
+
+    List<LintingStrategy> strategies = List.of(validArguments);
+    return new StrategiesContainer(trimNullStrategies(strategies));
+  }
+
+  private LintingStrategy getValidArguments(JsonObject jsonObject) {
+    String[] stringArgs =
+        jsonObject
+            .getAsJsonArray("arguments")
+            .toString()
+            .replace("[", "")
+            .replace("]", "")
+            .replace("\"", "")
+            .split(",");
+
+    List<AstNodeType> allowedArguments = new ArrayList<>();
+
+    for (String arg : stringArgs) {
+      allowedArguments.add(AstNodeType.valueOf(arg));
+    }
+
+    return new ArgumentsStrategy(allowedArguments);
+  }
+
+  private List<LintingStrategy> trimNullStrategies(List<LintingStrategy> strategies) {
+    List<LintingStrategy> trimmedStrategies = new ArrayList<>();
+    for (LintingStrategy strategy : strategies) {
+      if (strategy != null) {
+        trimmedStrategies.add(strategy);
+      }
+    }
+    return trimmedStrategies;
+  }
+}
