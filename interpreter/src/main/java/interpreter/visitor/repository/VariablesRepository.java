bbddcb81cc846a36c24622364dac560bfbeecd82
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-09-11 13:20:07 -0300

    Created a class that represents the key that will be saved in the varRepo. Added a factory that creates varId. Modified varRepo to work with this new class.

--- interpreter/src/main/java/interpreter/visitor/repository/VariablesRepository.java
+++ interpreter/src/main/java/interpreter/visitor/repository/VariablesRepository.java
@@ -2,24 +2,34 @@ package interpreter.visitor.repository;
 
 import ast.identifier.Identifier;
 import ast.literal.Literal;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class VariablesRepository {
   // Single map to store all variables
   private final Map<Identifier, Literal<?>> variables;
+  private final Map<VariableIdentifier, Literal<?>> newVariables;
 
   public VariablesRepository() {
-    this.variables = new HashMap<>();
+    this(new HashMap<>(), new HashMap<>());
   }
 
-  private VariablesRepository(Map<Identifier, Literal<?>> variables) {
-    this.variables = Collections.unmodifiableMap(new HashMap<>(variables));
+  public VariablesRepository(Map<Identifier, Literal<?>> variables) {
+    this(variables, new HashMap<>());
+  }
+
+  public VariablesRepository(
+      Map<Identifier, Literal<?>> variables, Map<VariableIdentifier, Literal<?>> newVariables) {
+    this.variables = Map.copyOf(variables);
+    this.newVariables = Map.copyOf(newVariables);
   }
 
   public Map<Identifier, Literal<?>> getVariables() {
     return new HashMap<>(variables);
+  }
+
+  public Map<VariableIdentifier, Literal<?>> getNewVariables() {
+    return newVariables;
   }
 
   @SuppressWarnings("unchecked")
@@ -32,8 +42,66 @@ public class VariablesRepository {
   }
 
   public VariablesRepository addVariable(Identifier identifier, Literal<?> value) {
-    Map<Identifier, Literal<?>> newVariables = new HashMap<>(variables);
+    Map<Identifier, Literal<?>> newVariables = new HashMap<>(getVariables());
     newVariables.put(identifier, value);
     return new VariablesRepository(newVariables);
+  }
+
+  public VariablesRepository addNewVariable(VariableIdentifier identifier, Literal<?> value) {
+    Map<VariableIdentifier, Literal<?>> newVariables = new HashMap<>(getNewVariables());
+    boolean isAlreadyDefined = newVariables.containsKey(identifier);
+
+    if (isAlreadyDefined) {
+      throw new IllegalArgumentException("Variable " + identifier + " is already defined.");
+    }
+
+    newVariables.put(identifier, value);
+
+    return new VariablesRepository(getVariables(), newVariables);
+  }
+
+  public VariablesRepository setNewVariable(VariableIdentifier identifier, Literal<?> value) {
+    Map<VariableIdentifier, Literal<?>> newVariables = new HashMap<>(getNewVariables());
+    VariableIdentifier keyId = getKeyId(identifier);
+
+    if (keyId == null) {
+      throw new IllegalArgumentException("Variable " + identifier.name() + " is not defined.");
+    }
+
+    if (!keyId.isModifiable()) {
+      throw new IllegalArgumentException("Variable " + identifier.name() + " is not modifiable.");
+    }
+
+    newVariables.put(keyId, value);
+    return new VariablesRepository(getVariables(), newVariables);
+  }
+
+  public <T> Literal<T> getNewVariable(VariableIdentifier identifier) {
+    Literal<T> value = (Literal<T>) getNewVariables().get(identifier);
+
+    if (value == null) {
+      throw new IllegalArgumentException("Variable " + identifier.name() + " is not defined.");
+    }
+
+    return value;
+  }
+
+  private VariableIdentifier getKeyId(VariableIdentifier identifier) {
+    for (VariableIdentifier key : newVariables.keySet()) {
+      if (key.equals(identifier)) {
+        return key;
+      }
+    }
+    return null;
+  }
+
+  public VariablesRepository update(VariablesRepository other) {
+    Map<VariableIdentifier, Literal<?>> mergedVariables = new HashMap<>(this.newVariables);
+    for (Map.Entry<VariableIdentifier, Literal<?>> entry : other.getNewVariables().entrySet()) {
+      if (mergedVariables.containsKey(entry.getKey())) {
+        mergedVariables.put(entry.getKey(), entry.getValue());
+      }
+    }
+    return new VariablesRepository(Map.of(), mergedVariables);
   }
 }
