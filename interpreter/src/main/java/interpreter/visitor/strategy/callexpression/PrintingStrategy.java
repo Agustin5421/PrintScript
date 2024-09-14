1b10f4cbf59ae7a894428652ce51950211374b5f
Elias85974 <128708960+Elias85974@users.noreply.github.com>
2024-09-14 18:59:22 -0300

    Printing strategy implemented

---
+++ interpreter/src/main/java/interpreter/visitor/strategy/callexpression/PrintingStrategy.java
@@ -0,0 +1,19 @@
+package interpreter.visitor.strategy.callexpression;
+
+import ast.literal.Literal;
+import ast.root.AstNode;
+import ast.visitor.NodeVisitor;
+import interpreter.visitor.OutputVisitor;
+import interpreter.visitor.strategy.InterpretingStrategy;
+import output.OutputResult;
+
+public class PrintingStrategy implements InterpretingStrategy {
+    @Override
+    public NodeVisitor interpret(AstNode node, NodeVisitor visitor) {
+        Literal<?> literal = (Literal<?>) node;
+        OutputVisitor outputVisitor = (OutputVisitor) visitor;
+        OutputResult<String> outputResult = outputVisitor.getOutputResult();
+        outputResult.saveResult(literal.value().toString());
+        return visitor;
+    }
+}
