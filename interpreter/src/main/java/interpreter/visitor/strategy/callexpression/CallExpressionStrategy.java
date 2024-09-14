9f619f47ca261819b496b39f2b714b59ca835a0a
Elias85974 <128708960+Elias85974@users.noreply.github.com>
2024-09-14 19:00:12 -0300

    CallExpressionStrategy implemented

---
+++ interpreter/src/main/java/interpreter/visitor/strategy/callexpression/CallExpressionStrategy.java
@@ -0,0 +1,26 @@
+package interpreter.visitor.strategy.callexpression;
+
+import ast.identifier.Identifier;
+import ast.root.AstNode;
+import ast.statements.CallExpression;
+import ast.visitor.NodeVisitor;
+import interpreter.visitor.strategy.InterpretingStrategy;
+import interpreter.visitor.strategy.StrategyContainer;
+
+public class CallExpressionStrategy implements InterpretingStrategy {
+    private final StrategyContainer<String> methodsContainer;
+
+    public CallExpressionStrategy(StrategyContainer<String> methodsContainer) {
+        this.methodsContainer = methodsContainer;
+    }
+
+    @Override
+    public NodeVisitor interpret(AstNode node, NodeVisitor visitor) {
+        CallExpression methodCall = (CallExpression) node;
+        Identifier methodName = methodCall.methodIdentifier();
+
+        InterpretingStrategy methodStrategy = methodsContainer.getStrategy(methodName.name());
+
+        return methodStrategy.interpret(node, visitor);
+    }
+}
