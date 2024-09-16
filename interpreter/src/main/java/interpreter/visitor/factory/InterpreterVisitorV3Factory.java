3160d37990cd01c8ac46c1988c529da23ed817b2
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-09-15 21:26:00 -0300

    Corrected the InterpreterVisitorV3Factory.

--- interpreter/src/main/java/interpreter/visitor/factory/InterpreterVisitorV3Factory.java
+++ interpreter/src/main/java/interpreter/visitor/factory/InterpreterVisitorV3Factory.java
@@ -5,8 +5,6 @@ import interpreter.ValueCollector;
 import interpreter.visitor.InterpreterVisitorV3;
 import interpreter.visitor.repository.VariablesRepository;
 import interpreter.visitor.strategy.StrategyContainer;
-import interpreter.visitor.strategy.callexpression.*;
-import interpreter.visitor.strategy.literal.*;
 import output.OutputResult;
 
 public class InterpreterVisitorV3Factory {
@@ -16,15 +14,20 @@ public class InterpreterVisitorV3Factory {
       new ValueCollectorStrategyFactory();
 
   public static InterpreterVisitorV3 getVisitor(String version, OutputResult<String> outputResult) {
-    StrategyContainer<AstNodeType> visitorStrategies =
-        mainVisitorStrategyFactory.createStrategyContainer();
+    StrategyContainer<AstNodeType> visitorStrategies;
+    StrategyContainer<AstNodeType> collectorStrategies;
 
-    StrategyContainer<AstNodeType> collectorStrategies =
-        switch (version) {
-          case "1.0" -> valueCollectorStrategyFactory.createStrategyContainerV1();
-          case "1.1" -> valueCollectorStrategyFactory.createStrategyContainerV2();
-          default -> throw new IllegalArgumentException("Invalid version: " + version);
-        };
+    switch (version) {
+      case "1.0" -> {
+        visitorStrategies = mainVisitorStrategyFactory.createStrategyContainerV1();
+        collectorStrategies = valueCollectorStrategyFactory.createStrategyContainerV1();
+      }
+      case "1.1" -> {
+        visitorStrategies = mainVisitorStrategyFactory.createStrategyContainerV2();
+        collectorStrategies = valueCollectorStrategyFactory.createStrategyContainerV2();
+      }
+      default -> throw new IllegalArgumentException("Invalid version: " + version);
+    }
 
     ValueCollector valueCollector = new ValueCollector(collectorStrategies);
 
--- interpreter/src/main/java/interpreter/visitor/factory/MainVisitorStrategyFactory.java
+++ interpreter/src/main/java/interpreter/visitor/factory/MainVisitorStrategyFactory.java
@@ -7,22 +7,40 @@ import interpreter.visitor.strategy.assignment.AssignmentExpressionStrategy;
 import interpreter.visitor.strategy.callexpression.CallExpressionStrategy;
 import interpreter.visitor.strategy.callexpression.PrintingStrategy;
 import interpreter.visitor.strategy.callexpression.PrintlnStrategy;
+import interpreter.visitor.strategy.conditional.IfStrategy;
 import interpreter.visitor.strategy.vardec.VariableDeclarationStrategy;
+import java.util.HashMap;
 import java.util.Map;
 
 public class MainVisitorStrategyFactory {
-  public StrategyContainer<AstNodeType> createStrategyContainer() {
+  public StrategyContainer<AstNodeType> createStrategyContainerV1() {
+    Map<AstNodeType, InterpretingStrategy> visitorStratMap = getCommonStrategies();
+
+    return new StrategyContainer<>(visitorStratMap);
+  }
+
+  public StrategyContainer<AstNodeType> createStrategyContainerV2() {
+    Map<AstNodeType, InterpretingStrategy> visitorStratMap = getCommonStrategies();
+
+    InterpretingStrategy ifStatementStrategy = new IfStrategy();
+
+    visitorStratMap.put(AstNodeType.IF_STATEMENT, ifStatementStrategy);
+
+    return new StrategyContainer<>(visitorStratMap);
+  }
+
+  private Map<AstNodeType, InterpretingStrategy> getCommonStrategies() {
     InterpretingStrategy varDecStrategy = new VariableDeclarationStrategy();
     InterpretingStrategy assignStrategy = new AssignmentExpressionStrategy();
     InterpretingStrategy callExpStrategy = getCallExpStrategy();
 
     Map<AstNodeType, InterpretingStrategy> visitorStratMap =
-        Map.of(
-            AstNodeType.VARIABLE_DECLARATION, varDecStrategy,
-            AstNodeType.ASSIGNMENT_EXPRESSION, assignStrategy,
-            AstNodeType.CALL_EXPRESSION, callExpStrategy);
-
-    return new StrategyContainer<>(visitorStratMap);
+        new HashMap<>(
+            Map.of(
+                AstNodeType.VARIABLE_DECLARATION, varDecStrategy,
+                AstNodeType.ASSIGNMENT_EXPRESSION, assignStrategy,
+                AstNodeType.CALL_EXPRESSION, callExpStrategy));
+    return visitorStratMap;
   }
 
   private InterpretingStrategy getCallExpStrategy() {
