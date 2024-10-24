2da80eb0ac3b7d69d8641ec50f11d8b81161e3a0
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-10-24 00:42:00 -0300

    Tweaks to OutputStringSystem.

--- utils/src/main/java/output/OutputStringSystem.java
+++ utils/src/main/java/output/OutputStringSystem.java
@@ -3,6 +3,7 @@ package output;
 public class OutputStringSystem implements OutputResult<String> {
   @Override
   public void saveResult(String result) {
+    System.out.println();
     System.out.println(result);
   }
 
